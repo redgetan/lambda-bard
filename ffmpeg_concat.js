b341a3b4858f916c5e667791e4c9dead8dcdd2cf
Reginald Tan <redge.tan@gmail.com>
2016-05-21 11:44:32 -0400

    init

---
+++ ffmpeg_concat.js
@@ -0,0 +1,19 @@
+'use strict';
+var exec = require('child_process').exec;
+
+exports.handler = (event, context, callback) => {
+  return callback("hello world");
+
+  if (!event.cmd) {
+      return callback('Please specify a command to run as event.cmd');
+  }
+
+  var child = exec(event.cmd, (error) => {
+      // Resolve with result of process
+      callback(error, 'Process complete!');
+  });
+
+  // Log process stdout and stderr
+  child.stdout.on('data', console.log);
+  child.stderr.on('data', console.error);
+};
