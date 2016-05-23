cfeff4805d0f5d5de875dbd8b510b232e72453b6
Reginald Tan <redge.tan@gmail.com>
2016-05-23 14:54:24 -0400

    mkdir -p build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir -p build
 zip --exclude '.git*' --exclude 'deploy.sh' --exclude 'build/*' --exclude 'events/*'  -r build/current.zip .
 aws lambda update-function-code --function-name ffmpeg_concat  --zip-file fileb:///`pwd`/build/current.zip
