aff5b9d31a898592ced12a815792fe48b3f9295e
Reginald Tan <redge.tan@gmail.com>
2016-05-23 11:06:12 -0400

    working ffmpeg_concat service

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip --exclude '*.git*' --exclude 'deploy.sh' --exclude '*build*'  -r build/current.zip .
+zip --exclude '*.git*' --exclude 'deploy.sh' --exclude '*build/*' -r build/current.zip .
 aws lambda update-function-code --function-name ffmpeg_concat  --zip-file fileb:///`pwd`/build/current.zip
--- ffmpeg_concat.js
+++ ffmpeg_concat.js
@@ -1,19 +1,129 @@
 'use strict';
 var exec = require('child_process').exec;
+var fs = require('fs');
+var AWS = require('aws-sdk');
+
+AWS.config.region = 'us-west-2';
+
+process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+
+// echo $URL_LIST | xargs -n 1 -P 8
+// "curl -o /tmp/lol.mp4 -s url"
+
+function buildVideoConcatCommand(segment_urls, outputFile) {
+  var binary = "./bin/ffmpeg"
+  var inputs = segment_urls.map(function(segment_url){ return " -i " + segment_url;  }).join(" ");
+  var concatFilterPre  = " -filter_complex \"";
+  var concatFilterMid  = segment_urls.map(function(segment_url, index){ return " [" + index + ":v] " + "[" + index + ":a] "; }).join("");
+  var concatFilterPost = "concat=n=" + segment_urls.length + ":v=1:a=1 [v] [a]\"" ;
+  var outputArgs = "-map '[v]' -map '[a]' -c:v libx264 -preset ultrafast -c:a aac -movflags +faststart " + outputFile;
+
+  return [binary, inputs, concatFilterPre, concatFilterMid, concatFilterPost, outputArgs].join(" ");
+}
+
+function getAlphaNumeric(text) {
+  return text.replace(/[^\w]+/g,"");
+}
+
 
 exports.handler = (event, context, callback) => {
-  return callback(null,"hello world");
+  var resultLocalPath = "/tmp/coming_591_100.mp4";
+  var url = "http://d22z4oll34c07f.cloudfront.net/segments/1yrHV5NBhc8/dnmt_62611_44.mp4";
+  // var list = [
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/this_453_100.mp4",
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/is_468_96.mp4",
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/a_480_42.mp4",
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/huge_488_95.mp4",
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/movie_526_100.mp4",
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/its_552_68.mp4",
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/going_565_100.mp4",
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/to_578_100.mp4",
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/be_584_100.mp4",
+  //   "http://d22z4oll34c07f.cloudfront.net/segments/n1IJt22JFY4/coming_591_100.mp4"
+  // ];
 
-  if (!event.cmd) {
-      return callback('Please specify a command to run as event.cmd');
-  }
+  var list = [
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/this_453_100.mp4",
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/is_468_96.mp4",
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/a_480_42.mp4",
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/huge_488_95.mp4",
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/movie_526_100.mp4",
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/its_552_68.mp4",
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/going_565_100.mp4",
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/to_578_100.mp4",
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/be_584_100.mp4",
+    "https://roplabs-mad.s3-us-west-2.amazonaws.com/segments/n1IJt22JFY4/coming_591_100.mp4"
+  ];
 
-  var child = exec(event.cmd, (error) => {
+  var localList = [
+    "/tmp/this_453_100.mp4",
+    "/tmp/is_468_96.mp4",
+    "/tmp/a_480_42.mp4",
+    "/tmp/huge_488_95.mp4",
+    "/tmp/movie_526_100.mp4",
+    "/tmp/its_552_68.mp4",
+    "/tmp/going_565_100.mp4",
+    "/tmp/to_578_100.mp4",
+    "/tmp/be_584_100.mp4",
+    "/tmp/coming_591_100.mp4"
+  ];
+
+  // var cmd = "curl -o " + resultLocalPath + " " + url;
+
+  var outputFile = Date.now() + ".mp4";
+  var outputFileAbsolutePath = "/tmp/" + outputFile;
+
+  var cmd = (typeof(event.queryParams) !== "undefined" && event.queryParams.cmd.length > 0)  ? event.queryParams.cmd : "cd /tmp && echo -e '" + list.join("\n") + "' | xargs -n 1 -P 8 curl -s -O";
+  var concatCmd = (typeof(event.queryParams) !== "undefined" && event.queryParams.cmd.length > 0) ? "" : buildVideoConcatCommand(localList, outputFileAbsolutePath);
+
+  var start;
+  var end;
+
+  start = new Date();
+
+  // console.log("cmd is " + event.query.cmd);
+  const child = exec(cmd, {maxBuffer: 1024 * 10000}, (error, stdout, stderr) => {
+      end = new Date();
+      console.log("fetchSegments took: " + (end - start));
       // Resolve with result of process
-      callback(error, 'Process complete!');
+      if (error) {
+        callback(error);
+      } 
+
+      if (concatCmd === "") {
+        return callback(null,stdout);
+      }
+
+      start = new Date();
+
+      exec(concatCmd, {maxBuffer: 1024 * 10000}, (error, stdout, stderr) => {
+
+        end = new Date();
+
+        console.log("concat took: " + (end - start));
+        if (error) {
+          callback(error);
+        } 
+
+        start = new Date();
+
+
+        var s3 = new AWS.S3();
+        var body     = fs.createReadStream(outputFileAbsolutePath);
+        var params = {
+          Bucket: 'roplabs-mad', 
+          Key: outputFile, 
+          ACL: "public-read", 
+          ContentType: "video/mp4",
+          Body: body
+        };
+
+        s3.upload(params, function (err, data) {
+          end = new Date();
+          console.log("s3 upload took: " + (end - start));
+          return callback(null,data.Location);
+        });
+      });
   });
 
-  // Log process stdout and stderr
-  child.stdout.on('data', console.log);
-  child.stderr.on('data', console.error);
 };
