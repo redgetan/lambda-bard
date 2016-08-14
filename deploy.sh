1cc7fa4f41de87c410519e4210ef5c8f2b994df6
Reginald Tan <redge.tan@gmail.com>
2016-08-13 23:18:10 -0400

    update deploy script to use alias

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 mkdir -p build
+rm build/* 
 zip --exclude '.git*' --exclude 'deploy.sh' --exclude 'build/*' --exclude 'events/*'  -r build/current.zip .
 aws lambda update-function-code --function-name ffmpeg_concat  --zip-file fileb:///`pwd`/build/current.zip
+version=`aws lambda publish-version --function-name ffmpeg_concat | jq -r .Version`
+aws lambda update-alias --function-name ffmpeg_concat --function-version $version --name PROD
