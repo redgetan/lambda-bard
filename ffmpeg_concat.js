e8a22633ef87747cade6dea7236f67019c58368d
Reginald Tan <redge.tan@gmail.com>
2016-09-06 19:54:25 -0400

    test raven on ffmpeg_concat

--- ffmpeg_concat.js
+++ ffmpeg_concat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var VideoMerger = require(__dirname + './lib/video_merger.js')
+var VideoMerger = require(__dirname + '/lib/video_merger.js')
 
 exports.handler = function(event, context, callback) {
   var videoMerger = new VideoMerger(event.queryParams);
