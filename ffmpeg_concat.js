a27b01424d294ace113b5106a6a9845adbec5187
Reginald Tan <redge.tan@gmail.com>
2016-09-06 20:19:37 -0400

    show version

--- ffmpeg_concat.js
+++ ffmpeg_concat.js
@@ -3,6 +3,10 @@
 var VideoMerger = require(__dirname + '/lib/video_merger.js')
 
 exports.handler = function(event, context, callback) {
+  if (event.queryParams.cmd === "show_version") {
+    context.done(null, "0.0.2");
+  }
+
   var videoMerger = new VideoMerger(event.queryParams);
 
   videoMerger.concatSegments().then(function(result){
--- test/events/basic_two.js
+++ test/events/basic_two.js
@@ -2,6 +2,8 @@
 module.exports = {
   queryParams: {
     text: "this:kTlq69V5vDg planet:K38RBkIHPK4 is:3aolAnVAXYE named:tEmrn5kAZOg after:aO9OGkbpxgw my:r1cuivtYiyk underwear:OhaFG45PLjY",
-    scene_token: "KbtlT_j3EDc"
+    scene_token: "KbtlT_j3EDc",
+    cmd: "show_version"
+
   }
 };
