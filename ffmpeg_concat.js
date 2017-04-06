edd2b6de93bb9809e9d00ba99d647d1e91d449b8
Reginald Tan <redge.tan@gmail.com>
2017-04-05 22:28:03 -0400

    authentication token being passed now from headers

--- ffmpeg_concat.js
+++ ffmpeg_concat.js
@@ -5,11 +5,15 @@ var VideoMerger = require(__dirname + '/lib/video_merger.js')
 exports.handler = function(event, context, callback) {
   if (event.queryParams.cmd === "show_version") {
     context.done(null, "0.0.3");
-  } else if (event.queryParams.cmd === "headers") {
-    var result = JSON.stringify(event.headers);
-    context.done(null, result);
   } else {
-    var videoMerger = new VideoMerger(event.queryParams);
+    var options = event.queryParams;
+
+    if ((typeof event.headers !== "undefined") && (typeof event.headers["Authorization"] !== "undefined")) {
+      var authorizationToken = event.headers["Authorization"].replace("Token ","");
+      options.authentication_token = authorizationToken;
+    }
+
+    var videoMerger = new VideoMerger(options);
 
     videoMerger.concatSegments().then(function(result){
       context.done(null, result);
