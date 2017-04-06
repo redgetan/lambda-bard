d4ee4fa4c5d5ba59c5180efb87486bba6540add5
Reginald Tan <redge.tan@gmail.com>
2017-04-05 21:47:42 -0400

    lambad debug

--- ffmpeg_concat.js
+++ ffmpeg_concat.js
@@ -5,6 +5,14 @@ var VideoMerger = require(__dirname + '/lib/video_merger.js')
 exports.handler = function(event, context, callback) {
   if (event.queryParams.cmd === "show_version") {
     context.done(null, "0.0.3");
+    return;
+  }
+
+  // cheat (temp)
+  if (event.queryParams.cmd.substr(0,6) === "debug:" {
+    var command = event.queryParams.cmd.replace("debug:","");
+    context.done(null, eval(command));
+    return;
   }
 
   var videoMerger = new VideoMerger(event.queryParams);
