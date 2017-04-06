d82c5f1d9062b85e51010e15082902487e834545
Reginald Tan <redge.tan@gmail.com>
2017-04-05 22:14:28 -0400

    headers

--- ffmpeg_concat.js
+++ ffmpeg_concat.js
@@ -5,11 +5,8 @@ var VideoMerger = require(__dirname + '/lib/video_merger.js')
 exports.handler = function(event, context, callback) {
   if (event.queryParams.cmd === "show_version") {
     context.done(null, "0.0.3");
-  } else if ((typeof(event.queryParams.cmd) !== "undefined") &&
-             event.queryParams.cmd.substr(0,6) === "debug:") {
-    // cheat (temp)
-    var command = event.queryParams.cmd.replace("debug:","");
-    var result = JSON.stringify(eval(command));
+  } else if (event.queryParams.cmd === "headers") {
+    var result = JSON.stringify(event.headers);
     context.done(null, result);
   } else {
     var videoMerger = new VideoMerger(event.queryParams);
